9eed4c4ada6c31d305070d9dcaa67fc1a99d62c8
KatsuDev <jensaki152@gmail.com>
2026-04-01 23:34:08 -0500

    Add files via upload
    Signed-off-by: KatsuDev <jensaki152@gmail.com>

--- Candy.py
+++ Candy.py
@@ -2,35 +2,43 @@
 
 import os, time, json, shutil
 
-ver: str = "0.0.2"
+ver: str = "0.0.3"
 
 DATA_FILE: str = "./Data/Data.json"
 LOG_DIR: str = "./Logs"
 CONFIG_FILE: str = "./Config.json"
 
-print(f"Candy Organizer v{ver} - 24/03/26 \nAutor: Kātsu Dev. jensaki52@gmail.com. Paypal: mitsuprojects3@gmail.com")
+
+print(r"  ____                _       ")
+print(r" / ___|__ _ _ __   __| |_   _ ")
+print(r"| |   / _` | '_ \ / _` | | | |")
+print(r"| |__| (_| | | | | (_| | |_| |")
+print(r" \____\__,_|_| |_|\__,_|\__, |")
+print(r"                        |___/ ")
+
+print(f"v{ver} - 01/04/26 \nAutor: Kātsu Dev. jensaki52@gmail.com. \nPaypal: mitsuprojects3@gmail.com")
 
 
 # ALL: Función de logs
 
 def log(msg):
     os.makedirs(LOG_DIR, exist_ok=True)
-    filename: str = time.strftime("%Y-%m-%d") + ".log"
+    filename: str = f"{time.strftime('%Y-%m-%d')}.log" # Formato del archivo
     path: str = os.path.join(LOG_DIR, filename)
 
-    with open(path, "a") as f:
-        f.write(f"[{time.strftime("%H:%M:%S")}] {msg}\n")
+    with open(path, "a") as f: # Escritura del archivo
+        f.write(f"[{time.strftime('%H:%M:%S')}] {msg}\n") # Formato del mensaje log
 
 # ALL: Leer y guardar
 
 def load():
     try:
-        if not os.path.exists(DATA_FILE):
-            return {}
+        if not os.path.exists(DATA_FILE): # Verifica si el archivo de data existe
+            return {} # Devuelve un diccionario vacio 
 
         with open(DATA_FILE, "r") as f:
             try:
-                return json.load(f)
+                return json.load(f) # Devuelve el archivo data cargado
             except json.JSONDecodeError as e:
                 log(e)
                 return {}
@@ -41,9 +49,25 @@ def load():
 
 def load_config():
     try: 
+        if not os.path.exists(CONFIG_FILE): 
+            config: dict = { # Opciones del diccionario
+                "format": "zip",
+                "delAfterPacked": True,
+                "every": 5,
+                "startOnExecute": False
+            }
+            
+            with open(CONFIG_FILE, "w") as cf:
+                try:
+                    json.dump(config, cf, indent = 4) # Genera un archivo nuevo si no existe
+                    log("Nuevo archivo de configuración creado.")
+                except Exception as e:
+                    print(f"Error fatal: {e}")
+                    log(e)
+            
         with open(CONFIG_FILE, "r") as c:
             try:
-                return json.load(c)
+                return json.load(c) # Devuelve el diccionario del archivo de configuración cargado
             except json.JSONDecodeError as e:
                 log(e)
                 
@@ -54,7 +78,7 @@ def load_config():
 def save(data):
     try:
         with open(DATA_FILE, "w") as f:
-            json.dump(data, f, indent=4)
+            json.dump(data, f, indent = 4) # Sobreescribe los datos nuevos
     
     except Exception as e:
             print(f"Error fatal: {e}")
@@ -62,24 +86,24 @@ def save(data):
 
 # ALL: Analizador de proyectos
 
-def check(data) -> int:
+def check(data):
     try:
         print("\nVerificando proyectos...")
-        errors: int = 0
+        errors: int = 0 # Variable de carpetas erroneas
 
-        for k, v in data.items():
-            if not os.path.exists(v["src"]):
-                print(f"Proyecto {k} no encontrado: {v["src"]}")
-                log(f"ERROR: Proyecto no encontrado {v["src"]}")
-                errors += 1
+        for k, v in data.items(): # Recorre data en busca de errores
+            if not os.path.exists(v['src']):
+                print(f"Proyecto {k} no encontrado: {v['src']}")
+                log(f"Error: Proyecto no encontrado {v['src']}")
+                errors += 1 # Lo añade a la lista
 
-            if not os.path.exists(v["backup"]):
-                print(f"Backup {k} no encontrado: {v["backup"]}")
-                log(f"ERROR: Backup no encontrado {v["backup"]}")
+            if not os.path.exists(v['backup']):
+                print(f"Backup {k} no encontrado: {v['backup']}")
+                log(f"Error: Backup no encontrado {v['backup']}")
                 errors += 1
 
         print(f"\nVerificación completa. {errors} errores.")
-        return errors
+        return errors # Devuelve el número de errores
     
     except Exception as e:
         print(f"Error fatal: {e}")
@@ -88,12 +112,12 @@ def check(data) -> int:
 def weight(path):
     size: int = 0
     try:
-        for dirpath, _, filenames in os.walk(path):
-            for file in filenames:
-                fp = os.path.join(dirpath, file)
+        for d, _, f in os.walk(path): # Recorre los directorios de forma recursiva 
+            for file in f:
+                fp = os.path.join(d, file)
                 if os.path.exists(fp):
-                    size += os.path.getsize(fp)
-        return size
+                    size += os.path.getsize(fp) # Captura el peso y lo suma
+        return size # Devuelve el peso total en bytes
 
     except Exception as e:
         print(f"Error fatal: {e}")
@@ -116,7 +140,8 @@ def add(data):
             "src": src,
             "backup": dst,
             "version": "1.0",
-            "weight": weight(src)
+            "weight": weight(src),
+            "pkgCount": 0
         }
 
         save(data)
@@ -135,7 +160,7 @@ def list_projects(data):
         return
 
     for k, v in data.items():
-        print(f"ID {k}: {v["src"]}, {v["weight"]}bytes. (v{v["version"]})")
+        print(f"ID {k}: {v['src']}, {v['weight']}bytes. (v{v['version']})")
 
 # ALL: Monitorea cuando un proyecto se actualiza
 
@@ -144,24 +169,24 @@ def run(data, f_data):
     try:
         while True:
             for k, v in data.items():
-                if not os.path.exists(v["src"]):
-                    log(f"ERROR: Ruta no encontrada {v["src"]}")
+                if not os.path.exists(v['src']):
+                    log(f"Error: Ruta no encontrada {v['src']}")
                     continue
 
-                w = weight(v["src"])
-                if w > v["weight"]:
-                    dest = os.path.join(v["backup"], v["version"])
+                w = weight(v['src'])
+                if w > v['weight']:
+                    dest = os.path.join(v['backup'], v['version'])
                     os.makedirs(dest, exist_ok=True)
 
-                    shutil.copytree(v["src"], dest, dirs_exist_ok=True)
+                    shutil.copytree(v['src'], dest, dirs_exist_ok=True)
 
-                    v["weight"] = w
+                    v['weight'] = w
                     save(data)
 
-                    log(f"Backup creado para {v["src"]} versión {v["version"]}")
-                    print(f"Backup actualizado: {v["src"]}")
+                    log(f"Backup creado para {v['src']} versión {v['version']}")
+                    print(f"Backup actualizado: {v['src']}")
 
-            time.sleep(f_data["every"])
+            time.sleep(f_data['every'])
 
     except KeyboardInterrupt:
         print("\nMonitoreo detenido.")
@@ -175,32 +200,35 @@ def run(data, f_data):
 
 def package(data, f_data):
     id = input("ID del proyecto: ")
-
+    
     try:
         if id not in data:
             print("ID inválido.")
             return
-
+        
         v: dict = data[id]
-        path: str = v["backup"]
+        name: str = f"{os.path.basename(v['src'])}_backup_{v['pkgCount']}"
+        path: str = v['backup']
 
         if not os.path.exists(path):
             print("No existe este directorio.")
             return
 
-        name: str = f"{os.path.basename(v["src"])}_backup"
-        shutil.make_archive(name, f_data["format"], path)
+        shutil.make_archive(name, f_data['format'], path)
         shutil.move(f"{name}.zip", path)
 
         log(f"Proyecto empaquetado: {name}")
         print("Empaquetado listo.")
 
-        if f_data["delAfterPacked"]:
+        v['pkgCount'] += 1
+        save(data)
+
+        if f_data['delAfterPacked']:
             log(f"Borrado automático")
-            for d in os.listdir(v["backup"]):
-                shutil.rmtree(os.path.join(v["backup"], d))
-                log(f"{os.path.join(v["backup"], d)} ha sido borrado.")
-                
+            for d in os.listdir(v['backup']):
+                shutil.rmtree(os.path.join(v['backup'], d))
+                log(f"{os.path.join(v['backup'], d)} ha sido borrado.")
+
     except NotADirectoryError:
         print("...")
 
@@ -215,29 +243,37 @@ def change(data):
 
     if id in data:
         try:
-            op: int = int(input(f"Escoge un parametro a editar. Editando: {os.path.abspath(data[id]["src"])}. \n - Origen(1) \n - Respaldo(2) \n - Versión(3) \n"))
+            op: int = int(input(f"Escoge un parametro a editar. Editando: {os.path.abspath(data[id]['src'])}. \n - Origen(1) \n - Respaldo(2) \n - Versión(3) \n"))
             if op < 1 or op > 3:
                 print("Debes elegir una de las opciones establecidas.")
             
             elif op == 1:
                 newd: str = input("Nuevo directorio a respaldar: ")
                 if os.path.exists(newd):
-                    data[id]["src"] = newd
+                    data[id]['src'] = newd
                     save(data)
                     print("Cambios guardados.")
-                    log(f"Origen de proyecto actualizado: {os.path.abspath(data[id]["src"])} ({data[id]["src"]})")
+                    log(f"Origen de proyecto actualizado: {os.path.abspath(data[id]['src'])} ({data[id]['src']})")
 
                 else:
                     print("Ruta inválida.")
                     return
             
             elif op == 2:
+                oldd: str = data[id]['backup']
                 newd: str = input("Nuevo directorio de respaldo: ")
+
                 if os.path.exists(newd):
-                    data[id]["backup"] = newd
-                    save(data)
-                    print("Cambios guardados.")
-                    log(f"Respaldo de proyecto actualizado: {data[id]["backup"]} ({data[id]["src"]})")
+                    try:
+                        data[id]['backup'] = newd
+                        save(data)
+                        shutil.copytree(oldd, newd, dirs_exist_ok=True)
+                        shutil.rmtree(oldd)
+
+                        print("Cambios guardados.")
+                        log(f"Respaldo de proyecto actualizado: {data[id]['backup']} ({data[id]['src']})")
+                    except Exception as e:
+                        print(f"Error fatal: {e}")
 
                 else:
                     print("Ruta inválida.")
@@ -245,10 +281,10 @@ def change(data):
             
             elif op == 3:
                 newv: str = input("Cambiar versión del respaldo: ")
-                data[id]["version"] = newv
+                data[id]['version'] = newv
                 save(data)
                 print("Cambios guardados")
-                log(f"Versión del proyecto actualizada: {os.path.abspath(data[id]["src"])} ({data[id]["version"]})")
+                log(f"Versión del proyecto actualizada: {os.path.abspath(data[id]['src'])} ({data[id]['version']})")
 
         except ValueError:
             print("Debes elegir una de las opciones establecidas.")
@@ -275,20 +311,22 @@ def main():
         log("Programa iniciado.")
 
         if not data:
-            print("No hay proyectos configurados.")
+            print("\nNo hay proyectos configurados.")
 
-        elif not f_data: 
-            print("Advertencia: No es posible cargar el archivo de configuración.")
-            log("No es posible cargar 'Config.json'")
+        # PARCHEADO
+        """elif not f_data: 
+            print("Advertencia: No es posible cargar el archivo de configuración, se creará uno nuevo.")
+            log("Error al cargar 'Config.json'")"""
 
         de: int = check(data)
         if de > 0:
-            op: str = input(f"Advertencia: Existen más de {de} directorios erroneos ¿Deseas continuar? (s/n): ").lower()
+            op: str = input(f"Advertencia: Existen {de} directorios erroneos ¿Deseas continuar? (s/n): ").lower()
             if op != "s":
                 return
 
         while True:
             cmd: str = input("\nIngresa un comando (help): ").lower()
+            cmd = cmd.replace(" ", "")
 
             if cmd == "add":
                 add(data)
@@ -296,7 +334,7 @@ def main():
             elif cmd == "list" or cmd == "ls":
                 list_projects(data)
 
-            elif cmd == "run" or f_data["startOnExecute"]:
+            elif cmd == "run" or f_data['startOnExecute']:
                 run(data, f_data)
                 log("Inicio automático.")
 
--- README.md
+++ README.md
@@ -2,43 +2,30 @@
 
 Usalo y editalo como desees
 
-Este es un programa de consola, es decir que no cuenta con GUI.
+Este es un programa de consola, es decir que no cuenta con GUI. diseñado para ser simple y ligero.
 Funciona como un especie de organizador, guarda una carpeta y copia su contenido en otra, categorizado por versiones. 
-Está pensado para usarse con almacenamiento externo, como un usb o una targeta de memoria; tambien se puede usar con almacenamiento interno pero no tendria sentido.
-
-Hay pequeños problemas con el empaquetado por ejemplo, solo guarda un zip por versión, en lugar de reemplazarlo, pero no crashea.
-Tampoco puedes borrar proyectos y solo empieza a respaldar archivos luego de ejecutar run. 
+Está pensado para usarse con almacenamiento externo, como un usb o una targeta de memoria.
 
 Al iniciar el ejecutable se recorrerá todo el diccionario de proyectos, y verificará que los directorios configurados existan; si no, saltará una advertencia y preguntará si deseas continuar.
 
-#### 0.0.2:
+#### 0.0.3:
 
 **Historial de cambios:**
 
-- *Archivo de configuración añadido:* Puedes cambiar comportamientos del programa cambiando los valores en el archivo `Config.json`:
-```
-format: Tipo de empaquetado. (str, 'zip' por defecto.)
-delAfterPacked: Borrar despues de empaquetar. (bool, 'true' por defecto.)
-every: Tiempo de cada actualización del backup. (int/float, '5' por defecto.)
-startOnExecute: Iniciar automáticamente despues de ejecutar el programa. (bool, 'false' por defecto.)
-```
+- *Cambios en change/ch:* Ahora mueve los archivos al cambiar la ruta de backup.
 
-- *Nuevos logs agregados:* Entre ellos, logs de inicio y finalización del programa, excepciones en `main()` y `run()`.
+- *Cambios en package/pkg:* Ahora se podrá empaquetar los backups múltiples veces, sin embargo se empaquetará todo, incluyendo otros backups.
 
-- *Más excepciones.*
+- *Cambios en Data.json:* Se añadió el parámetro `pkgCount`, donde se guardarán registros de cuantas veces el backup se ha empaquetado.
 
-- *Excepción de salida en main:* Ctr+c`.
+- *Cambios al leer la configuración:* Ahora al leer `Config.json` se verificará que el archivo exista, caso contrario se generará un archivo nuevo.
+**Cuidado:** Esto generará las configuraciones por defecto.
 
-- *Alternativas al escribir comandos:*
-```
-- list/ls
-- package/pkg
-- change/ch
-```
+- *Cambios en la lectura de comandos:* Ahora no importará si dejas un espacio al final de cada comando, se leerá igual.
 
-- *Función 'check' agregada.*
+- *Advertencia al no poder leer el archivo de configuración removida.* 
 
-- *Función 'help' agregada.*
+- *Cambios menores de estética y escritura del código*
 
 #### USO:
 
@@ -53,7 +40,7 @@ Espera los args:
 Se asignará por defecto la versión 1.0, y creará un directorio en el directorio de respaldo con la versión como nombre al actualizar, donde se guardarán los cambios hasta que cambies la versión.
 También se les asignará una id de referencia como claves en el diccionario.
 
-**change:** Cambia los datos del proyecto en el diccionario.
+**change/ch:** Cambia los datos del proyecto en el diccionario.
 ```
 Espera los args:
 !ID del proyecto (int)
@@ -61,24 +48,31 @@ Espera los args:
 -*Directorio de respaldo (str)
 -*Versión (str)
 ```
-*Cuidado:* Esto no mueve los directorios si se cambia su dirección de respaldo.
 Puedes cambiar a una versión anterior sin problemas, se segurá editando el directorio de esa versión si el programa lo está monitoreando.
 
-**package:** Empaqueta todos los archivos en el directorio del backup.
+**package/pkg:** Empaqueta todos los archivos en el directorio del backup.
 ```
 Espera los args:
 !ID del proyecto (int)
 ```
-Se empaquetarán con el formato configurado en el archivo `Config.json`, y si está configurado tambien se borrarán los demás directorios dejando solo el archivo configurado.
+Se empaquetarán con el formato configurado en el archivo `Config.json`, y si está configurado se limpiarán los demás directorios, dejando solo el empaquetado.
 
-**list:** Muestra la lista de proyectos y su información.
+**list/ls:** Muestra la lista de proyectos y su información.
 
 **check:** Verifica si los directorios configurados existen.
 
-**run:** Empieza a verificar cada 5seg cambios en los proyectos configurados.
-Si está configurado en el archivo `Config.json`, se iniciará automáticamente al ejecutar el programa.
+**run:** Verifica cambios en el directorio de origen y actualiza el backup.
+Si está configurado en el archivo `Config.json`, se iniciará automáticamente al ejecutar el programa; de igual manera dependiendo de la configuración, la frecuencia de monitoreo cambiará.
 
-Se guardarán reportes de cada actualización y cada error en la carpeta de Logs. La carpeta Data.json contiene los diccionarios de cada proyecto.
+#### CONFIGURACIÓN:
+```
+format: Tipo de empaquetado. (str, 'zip' por defecto.)
+delAfterPacked: Borrar después de empaquetar. (bool, 'true' por defecto.)
+every: frecuencia de actualización actualización del backup. (int/float, '5' por defecto.)
+startOnExecute: Iniciar automáticamente al ejecutar el programa. (bool, 'false' por defecto.)
+```
+
+Se guardarán reportes de cada actualización y cada error en la carpeta de Logs. El archivo Data.json contiene los diccionarios de cada proyecto.
 
 Actualizaré esto de vez en cuando; solo hice un binario para linux, pero de nuevo, pueden hacer con esto lo que deseen.
 
